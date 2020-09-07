cbc48caaa09e738b2001639a58075b06bc16e415
Aida Real <areal060781@gmail.com>
2020-09-07 12:05:09 -0500

    Display messages in the web browser

--- temp_messenger/service.py
+++ temp_messenger/service.py
@@ -1,23 +1,9 @@
 from nameko.rpc import rpc, RpcProxy
 from nameko.web.handlers import http
+from werkzeug.wrappers import Response
+
 from .dependencies.redis import MessageStore
-
-
-class KonnichiwaService:
-    name = 'konnichiwa_service'
-
-    @rpc
-    def konnichiwa(self):
-        return 'Konnichiwa!'
-
-
-class WebServer:
-    name = 'web_server'
-    konnichiwa_service = RpcProxy('konnichiwa_service')
-
-    @http('GET', '/')
-    def home(self, request):
-        return self.konnichiwa_service.konnichiwa()
+from .dependencies.jinja2 import Jinja2
 
 
 class MessageService:
@@ -37,3 +23,21 @@ class MessageService:
     def get_all_messages(self):
         messages = self.message_store.get_all_messages()
         return messages
+
+
+class WebServer:
+    name = 'web_server'
+    message_service = RpcProxy('message_service')
+    templates = Jinja2()
+
+    @http('GET', '/')
+    def home(self, request):
+        messages = self.message_service.get_all_messages()
+        rendered_template = self.templates.render_home(messages)
+        html_response = create_html_response(rendered_template)
+        return html_response
+
+
+def create_html_response(content):
+    headers = {'Content-Type': 'text/html'}
+    return Response(content, status=200, headers=headers)
