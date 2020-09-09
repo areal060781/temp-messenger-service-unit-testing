5818cbd248a88000fa9b442d92354e87956673e0
Aida Real <areal060781@gmail.com>
2020-09-09 11:42:23 -0500

    Split the message and web services

--- temp_messenger/service.py
+++ temp_messenger/service.py
@@ -1,32 +1,10 @@
 import json
-from operator import itemgetter
 
 from nameko.rpc import rpc, RpcProxy
 from nameko.web.handlers import http
 from werkzeug.wrappers import Response
 
-from .dependencies.redis import MessageStore
 from .dependencies.jinja2 import Jinja2
-
-
-class MessageService:
-    name = 'message_service'
-    message_store = MessageStore()
-
-    @rpc
-    def get_message(self, message_id):
-        return self.message_store.get_message(message_id)
-
-    @rpc
-    def save_message(self, message):
-        message_id = self.message_store.save_message(message)
-        return message_id
-
-    @rpc
-    def get_all_messages(self):
-        messages = self.message_store.get_all_messages()
-        sorted_messages = sort_messages_by_expiry(messages)
-        return sorted_messages
 
 
 class WebServer:
@@ -68,14 +46,6 @@ class WebServer:
 def create_html_response(content):
     headers = {'Content-Type': 'text/html'}
     return Response(content, status=200, headers=headers)
-
-
-def sort_messages_by_expiry(messages, reverse=False):
-    return sorted(
-        messages,
-        key=itemgetter('expires_in'),
-        reverse=reverse
-    )
 
 
 def create_json_response(content):
